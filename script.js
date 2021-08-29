0aa7b4ff5ddb1fc31924b9a9bdc32113a05d53e9
Erik Garcia Tacas <erik.fisi16@gmail.com>
2021-08-28 23:24:52 -0500

    :lipstick: feat: add style in sections

---
+++ script.js
@@ -0,0 +1,38 @@
+const icon = document.getElementById("icon")
+const navbarLinks = document.getElementById('navbar__links')
+
+window.onscroll = function () {
+    let top = window.pageYOffset || document.documentElement.scrollTop
+    if(top > 0) {
+            document.getElementsByClassName('header')[0].classList.add('shadow')
+    }else {
+        document.getElementsByClassName('header')[0].classList.remove('shadow')
+    }
+}
+
+
+icon.addEventListener("click", () => {
+    const classList = icon.className.split(' ')
+    let burguer = false
+    for (const _class of classList) {
+        if(_class === 'icon-burger'){
+            burguer = true
+        }
+    }
+    if(burguer){
+        icon.classList.remove('icon-burger')
+        icon.classList.add('icon-cancel')
+        navbarLinks.classList.add('show')
+
+    }else {
+        icon.classList.remove('icon-cancel')
+        icon.classList.add('icon-burger')
+        navbarLinks.classList.remove('show')
+    }
+})
+
+function hiddenNavbarLinks() {
+    navbarLinks.classList.remove('show')
+    icon.classList.remove('icon-cancel')
+    icon.classList.add('icon-burger')
+}
